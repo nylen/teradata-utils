e260b17be14f5e092dd1b5f7950f623853fdaaf1
James Nylen <jnylen@gmail.com>
2011-06-07 12:56:14 -0400

    Align spool space and usage fields

--- Get personal spool space & usage.sql
+++ Get personal spool space & usage.sql
@@ -1,11 +1,11 @@
 SEL vproc,
 	SUM(currentspool) / (1024**3) (DECIMAL(10,5)) AS gb_current_spool,
-	SUM(peakspool) / (1024**3) (DECIMAL(10,5)) AS gb_peak_spool,
-	SUM(maxspool) / (1024**3) (DECIMAL(10,5)) AS gb_max_spool,
-	SUM(currenttemp) / (1024**3) (DECIMAL(10,5)) AS gb_current_temp,
-	SUM(maxtemp) / (1024**3) (DECIMAL(10,5)) AS gb_max_temp,
-	SUM(currentperm) / (1024**3) (DECIMAL(10,5)) AS gb_curr_perm,
-	SUM(maxperm) / (1024**3) (DECIMAL(10,5)) AS gb_max_perm 
+	SUM(peakspool)    / (1024**3) (DECIMAL(10,5)) AS gb_peak_spool,
+	SUM(maxspool)     / (1024**3) (DECIMAL(10,5)) AS gb_max_spool,
+	SUM(currenttemp)  / (1024**3) (DECIMAL(10,5)) AS gb_current_temp,
+	SUM(maxtemp)      / (1024**3) (DECIMAL(10,5)) AS gb_max_temp,
+	SUM(currentperm)  / (1024**3) (DECIMAL(10,5)) AS gb_curr_perm,
+	SUM(maxperm)      / (1024**3) (DECIMAL(10,5)) AS gb_max_perm 
 FROM dbc.diskspace 
 WHERE databasename = 'ajb08' 
 --WHERE databasename = 'aa'
