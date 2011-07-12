fb9ab460d4d52196405fe2dbff3b46e60cb5c6e2
James Nylen <jnylen@gmail.com>
2011-07-12 16:32:25 -0400

    Handle long column names and missing columns
    Teradata column names can't be longer than 30 bytes.  Since we add 5
    bytes to each column name ("diff_ColumnName"), if the column name is
    longer than 25 bytes, concatenate the first 24 bytes of the name with
    (ColumnIndex MOD 10).
    
    Also, only attempt to compare columns that appear in both tables.

--- Generate SQL to compare two tables.sql
+++ Generate SQL to compare two tables.sql
@@ -1,28 +1,46 @@
-/* Generate a query which will compare two tables.
- * The tables must have the same columns, and they
- * must both have a unique primary index that
- * references the same columns.
+/* Generate a query which will compare the columns
+ * two tables have in common.  The tables must both
+ * have a unique primary index that references the
+ * same columns.
  */
 
 CREATE VOLATILE TABLE cols AS (
-	SEL TRIM(c.columnname) col, colindex,
+	SEL TRIM(ca.columnname) col,
+	    
+	    CASE
+	        WHEN CHARS(col) > 25
+	            THEN SUBSTR(col,1,24) || TRIM(colindex MOD 10)
+	        ELSE col
+	    END AS colshort,
+	    
+	    colindex,
 		'N' firstcol, 'N' lastcol,
 		'N' firstindex, 'N' lastindex,
 		'N' skipcmp,
+		
 		CASE
 			WHEN indextype = 'P' AND uniqueflag = 'Y' THEN 'Y'
 			ELSE 'N'
 		END AS isindex
+	    
 	FROM (
 		SEL databasename, tablename, columnname,
-			ROW_NUMBER() OVER(ORDER BY columnid) colindex
+    	    ROW_NUMBER() OVER(ORDER BY columnid) colindex,
 		FROM dbc.COLUMNS
 		WHERE TRIM(databasename) || '.' || TRIM(tablename) = '?table_a'
-	) c
+	) ca
+	
+	INNER JOIN (
+		SEL columnname
+		FROM dbc.COLUMNS
+		WHERE TRIM(databasename) || '.' || TRIM(tablename) = '?table_b'
+	) cb
+	ON ca.columnname = cb.columnname
+	
 	LEFT JOIN dbc.indices i
-	ON i.databasename = c.databasename
-	AND i.tablename = c.tablename
-	AND i.columnname = c.columnname
+	ON i.databasename = ca.databasename
+	AND i.tablename = ca.tablename
+	AND i.columnname = ca.columnname
 ) WITH DATA ON COMMIT PRESERVE ROWS;
 
 
@@ -32,14 +50,15 @@ CREATE VOLATILE TABLE cols AS (
 
 
 CREATE VOLATILE TABLE cols_count AS (
-	SEL COUNT(*) colcount,
-		SUM(CASE WHEN isindex = 'Y' THEN 1 ELSE 0 END) AS indexcount
+	SEL COUNT(*) colcount, MAX(colindex) maxindex
 	FROM cols
 ) WITH DATA ON COMMIT PRESERVE ROWS;
 
-UPDATE cols SET firstcol = 'Y' WHERE colindex = 1;
+UPDATE cols SET firstcol = 'Y' WHERE colindex IN (
+    SEL MIN(colindex) FROM cols
+);
 UPDATE cols SET lastcol = 'Y' WHERE colindex IN (
-	SEL colcount FROM cols_count
+	SEL MAX(colindex) FROM cols
 );
 UPDATE cols SET firstindex = 'Y' WHERE colindex IN (
 	SEL MIN(colindex) FROM cols
@@ -65,23 +84,23 @@ UNION ALL SEL 10, 5, '', 'SEL' FROM cols_count
 UNION ALL SEL 20, colindex, col,
 	'    ' || CASE WHEN firstcol = 'Y' THEN '' ELSE '+ ' END ||
 	CASE WHEN skipcmp = 'Y' THEN '0--' ELSE '' END ||
-	'CASE WHEN diff_' || col || ' = ''Y'' THEN 1 ELSE 0 END'
+	'CASE WHEN diff_' || colshort || ' = ''Y'' THEN 1 ELSE 0 END'
 FROM cols
 
 UNION ALL SEL 30, 1, '', '    AS _num_mismatches,' FROM cols_count
 UNION ALL SEL 30, 2, '', '    ' FROM cols_count
 UNION ALL SEL 30, 3, '', '    CASE' FROM cols_count
-UNION ALL SEL 30, 4, '', '        WHEN ' || col || '_b' ||
+UNION ALL SEL 30, 4, '', '        WHEN ' || colshort || '_b' ||
 	' IS NULL THEN ''(Row only in ?table_a)''' FROM cols WHERE firstindex = 'Y'
-UNION ALL SEL 30, 5, '', '        WHEN ' || col || '_a' ||
+UNION ALL SEL 30, 5, '', '        WHEN ' || colshort || '_a' ||
 	' IS NULL THEN ''(Row only in ?table_b)''' FROM cols WHERE firstindex = 'Y'
 UNION ALL SEL 30, 6, '', '        ELSE TRIM(TRAILING '','' FROM TRIM(' FROM cols_count
 
 UNION ALL SEL 40, colindex, col,
 	'            ' || CASE WHEN firstcol = 'Y' THEN '' ELSE '|| ' END ||
 	CASE WHEN skipcmp = 'Y' THEN '''''--' ELSE '' END ||
-	'CASE WHEN diff_' || col || ' = ''Y'' THEN ''' || col || ' (' ||
-		TRIM(colindex) || '/' || TRIM(colcount) || '), '' ELSE '''' END'
+	'CASE WHEN diff_' || colshort || ' = ''Y'' THEN ''' || col || ' (' ||
+		TRIM(colindex) || '/' || TRIM(maxindex) || '), '' ELSE '''' END'
 FROM cols
 INNER JOIN cols_count
 ON 1 = 1
@@ -91,13 +110,13 @@ UNION ALL SEL 50, 2, '', '    END AS _mismatches,' FROM cols_count
 UNION ALL SEL 50, 3, '', '    ' FROM cols_count
 
 UNION ALL SEL 60, colindex, col,
-	'    a.' || col || ' ' || col || '_a, ' ||
-	'b.' || col || ' ' || col || '_b, ' ||
+	'    a.' || col || ' ' || colshort || '_a, ' ||
+	'b.' || col || ' ' || colshort || '_b, ' ||
 	'CASE '
 		|| 'WHEN a.' || col || ' = b.' || col
 		|| ' OR (a.' || col || ' IS NULL AND b.' || col || ' IS NULL)'
 		|| ' THEN ''N'' ELSE ''Y'' ' ||
-	'END AS diff_' || col || CASE WHEN lastcol = 'Y' THEN '' ELSE ',' END
+	'END AS diff_' || colshort || CASE WHEN lastcol = 'Y' THEN '' ELSE ',' END
 FROM cols
 
 UNION ALL SEL 70, 1, '', '' FROM cols_count
@@ -116,14 +135,14 @@ UNION ALL SEL 90, 1, '', '' FROM cols_count
 UNION ALL SEL 100, colindex, col,
 	CASE WHEN firstcol = 'Y' THEN 'WHERE' ELSE 'OR' END ||
 	CASE WHEN skipcmp = 'Y' THEN ' 1=0--' ELSE '' END ||
-	' diff_' || col || ' = ''Y'''
+	' diff_' || colshort || ' = ''Y'''
 FROM cols
 
 UNION ALL SEL 110, 1, '', '' FROM cols_count
 UNION ALL SEL 110, 2, '', 'ORDER BY' FROM cols_count
 
 UNION ALL SEL 120, colindex, col,
-	'COALESCE(' || col || '_a, ' || col || '_b)' ||
+	'COALESCE(' || colshort || '_a, ' || colshort || '_b)' ||
 	CASE WHEN lastindex = 'Y' THEN '' ELSE ',' END
 FROM cols
 WHERE isindex = 'Y'
